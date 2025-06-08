691d0cd656972c916b2b81c2cff193540a65250d
mingu <mc12alsrn@gmail.com>
2025-06-08 22:20:08 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+    const faders = document.querySelectorAll('.fade-in');
+
+    const appearOptions = {
+        threshold: 0.2, // 뷰포트의 20%가 보일 때 애니메이션 시작
+        rootMargin: "0px 0px -50px 0px" // 뷰포트 하단에서 50px 위에서 트리거
+    };
+
+    const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return;
+            } else {
+                entry.target.classList.add('active');
+                appearOnScroll.unobserve(entry.target); // 한 번만 실행되도록
+            }
+        });
+    }, appearOptions);
+
+    faders.forEach(fader => {
+        appearOnScroll.observe(fader);
+    });
+});
